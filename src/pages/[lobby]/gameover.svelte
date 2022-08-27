cd657cd91b100423ad06e0b7ad7df4c90d9fcc81
Geoffrey Mugnier <geo.mugnier13@gmail.com>
2022-08-27 17:04:37 +0200

    fix: gameover layout

--- src/pages/[lobby]/gameover.svelte
+++ src/pages/[lobby]/gameover.svelte
@@ -1,20 +1,10 @@
 <script>
   import { goto } from '@roxi/routify';
-  import { onMount } from 'svelte';
   import Logs from '../../lib/Logs.svelte';
-  import room from '../../room';
-
-  export let lobby;
-  
-  onMount(() => {
-    room.reset();
-  })
 </script>
 
 <h1>😭😭 Game Over 😭😭</h1>
-<div class="flex">
-  <button on:click={() => $goto(`/${lobby}`)} class="btn btn-primary-outlined">Retourner au lobby</button>
-</div>
+<button on:click={() => $goto(`/`)} class="btn btn-primary-outlined">Retourner au lobby</button>
 
 <button class="bg-gray-50 mt-5 inline-block lg:hidden" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
   Voir l'historique ›
